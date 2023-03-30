d020c5d6674ae0fbd00434d14f8bc3cd1b016bd4
nome <nome>
2023-03-30 15:03:38 -0300

    segund commit

---
+++ src/main/java/br/com/ifpe/oxefood/OxefoodApiJosimeyrissonApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxefoodApiJosimeyrissonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxefoodApiJosimeyrissonApplication.class, args);
+	}
+
+}
