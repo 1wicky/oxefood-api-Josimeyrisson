a0f49d088462fad54079a321e0decc1da33981b5
nome <nome>
2023-04-14 15:19:05 -0300

    quarto commit

--- src/main/java/br/com/ifpe/oxefood/util/entity/EntidadeAuditavel.java
+++ src/main/java/br/com/ifpe/oxefood/util/entity/EntidadeAuditavel.java
@@ -2,6 +2,15 @@ package br.com.ifpe.oxefood.util.entity;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Version;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import lombok.Getter;
 import lombok.Setter;
 
--- src/main/java/br/com/ifpe/oxefood/util/entity/EntidadeNegocio.java
+++ src/main/java/br/com/ifpe/oxefood/util/entity/EntidadeNegocio.java
@@ -2,6 +2,14 @@ package br.com.ifpe.oxefood.util.entity;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
